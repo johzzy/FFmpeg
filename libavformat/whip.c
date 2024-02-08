6b8e70ee8adea49661913cc4f7c994384844305a
Johnny <hellojinqiang@gmail.com>
2024-02-08 20:51:04 +0800

    feat(whip): case   switch (whip->flags)

--- libavformat/whip.c
+++ libavformat/whip.c
@@ -687,6 +687,8 @@ static int generate_sdp_offer(AVFormatContext *s)
         if (whip->audio_par->codec_id == AV_CODEC_ID_OPUS)
             acodec_name = "opus";
 
+        switch (whip->flags) {
+        case kDtlsSrtpKeyAgreement:
         av_bprintf(&bp, ""
             "m=audio 9 UDP/TLS/RTP/SAVPF %u\r\n"
             "c=IN IP4 0.0.0.0\r\n"
@@ -713,7 +715,9 @@ static int generate_sdp_offer(AVFormatContext *s)
             whip->audio_par->ch_layout.nb_channels,
             whip->audio_ssrc,
             whip->audio_ssrc);
+            break;
 
+        case kPlaintextAgreement:
         av_bprintf(&bp, ""
                         "m=audio 9 UDP/RTP %u\r\n"
                         "c=IN IP4 0.0.0.0\r\n"
@@ -739,6 +743,10 @@ static int generate_sdp_offer(AVFormatContext *s)
                    whip->audio_par->ch_layout.nb_channels,
                    whip->audio_ssrc,
                    whip->audio_ssrc);
+            break;
+
+        default: abort();
+        }
     }
 
     if (whip->video_par) {
@@ -750,6 +758,8 @@ static int generate_sdp_offer(AVFormatContext *s)
             profile_idc = whip->video_par->profile & 0x00ff;
         }
 
+        switch (whip->flags) {
+        case kDtlsSrtpKeyAgreement:
         av_bprintf(&bp, ""
             "m=video 9 UDP/TLS/RTP/SAVPF %u %u\r\n"
             "c=IN IP4 0.0.0.0\r\n"
@@ -791,7 +801,9 @@ static int generate_sdp_offer(AVFormatContext *s)
             whip->video_rtx_ssrc,
             whip->video_ssrc,
             whip->video_ssrc);
+                break;
 
+        case kPlaintextAgreement:
         av_bprintf(&bp, ""
                         "m=video 9 UDP/RTP %u\r\n"
                         "c=IN IP4 0.0.0.0\r\n"
@@ -821,6 +833,10 @@ static int generate_sdp_offer(AVFormatContext *s)
                    level,
                    whip->video_ssrc,
                    whip->video_ssrc);
+            break;
+
+        default: abort();
+        }
     }
 
     if (!av_bprint_is_complete(&bp)) {
