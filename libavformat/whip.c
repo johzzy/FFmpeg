8ceb816c4881b27c4cabcd47b7e350df6395f2d5
Johnny <hellojinqiang@gmail.com>
2024-06-24 11:15:03 +0800

    fix build whip

--- libavformat/whip.c
+++ libavformat/whip.c
@@ -1578,7 +1578,7 @@ static int on_rtp_write_packet(void *opaque, const uint8_t *buf, int buf_size)
     return ret;
 }
 
-static int on_rtp_write_packet_plaintext(void *opaque, uint8_t *buf, int buf_size)
+static int on_rtp_write_packet_plaintext(void *opaque, const uint8_t *buf, int buf_size)
 {
     int ret, is_rtcp;
     uint8_t payload_type;
